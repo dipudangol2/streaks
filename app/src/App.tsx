fc1a8480739d1a89fa744a31833f8bb5c781cf0c
dipudangol2 <dipudangol2@gmail.com>
2025-11-24 22:39:53 +0545

    Setup shadcn and import field components for form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,14 @@
-import Login from "./components/Login";
 import { Navigate, Route,BrowserRouter as Router,Routes } from "react-router-dom";
-import Signup from "./components/Signup";
 import Home from "./pages/Home";
+import { Auth } from "./pages/Auth";
 
 const App = () => {
   return (
    <Router>
     <Routes>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
       <Route path="/" element={<Home/>}/>
+      <Route path="/auth" element={<Auth/>}/>
       <Route path="*" element={<Navigate to="/"/>}/>
-      
     </Routes>
    </Router>
    
---
+++ app/src/pages/Login/index.tsx
@@ -0,0 +1,19 @@
+import {
+  FieldSet,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  Field
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+
+export const Auth = () => {
+  return (
+    <FieldSet>
+      
+    </FieldSet>
+  )
+};
