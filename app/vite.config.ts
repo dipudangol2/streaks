5608ab51566f8e965621b13877cef3ca0df09d9c
dipudangol2 <dipudangol2@gmail.com>
2026-03-30 21:33:30 +0545

    feat[Docker]:Containerize the application with a multi-container setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,10 +6,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname, "./src"),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     }
-  }
-
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
 })
